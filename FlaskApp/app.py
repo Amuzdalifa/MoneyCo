1c252e9400f820e1a75ba65cacfaffcb0383dd7b
muthmainnah234 <muthmainnah234@gmail.com>
2017-11-28 23:54:47 +0700

    tampilan

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return render_template('coba.html')
+    return render_template('index.html')
 
 @app.route('/predict',methods=['POST'])
 def count():
